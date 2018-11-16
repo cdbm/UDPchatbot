b16f07ba92eb0dd961e2f8acb0322804ab07cca6
Christian Davi <32940205+cdbm@users.noreply.github.com>
2018-11-16 00:19:00 -0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -14,15 +14,17 @@ public class Client {
 		String msg = "";
 
 		while (true && !msg.equals("end")) {
+			byte[] receiveData = new byte[500];
 			msg = in.nextLine();
 			if (!msg.equals("end")) {
 				sendData = (msg).getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPServer, 5000);
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPServer, 3000);
 				long t1 = System.nanoTime();
 				clientSocket.send(sendPacket);
-				byte[] receiveData = new byte[1];
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				clientSocket.receive(receivePacket);
+				String msgS = new String (receiveData, "UTF-8");
+				System.out.println(msgS);
 				long t2 = System.nanoTime();
 				System.out.println("T1 " + (t1 / 1000));
 				System.out.println("T2 " + (t2 / 1000));
