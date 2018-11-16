5c6fedcf190f37bc0912792c1b4085d1484f517f
Christian Davi <32940205+cdbm@users.noreply.github.com>
2018-11-16 00:19:32 -0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,18 +6,29 @@ import java.net.InetAddress;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
-		DatagramSocket serverSocket = new DatagramSocket(5000);
-		byte[] receiveData = new byte[1000];
+		DatagramSocket serverSocket = new DatagramSocket(3000);
 		byte[] sendData;
 		InetAddress clientIP;
 		int port;
+		String resp = "";
 		while (true) {
+			byte[] receiveData = new byte[100];
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
 			clientIP = receivePacket.getAddress();
 			port = receivePacket.getPort();
-			sendData = ("2").getBytes();
 			String msgS = new String (receiveData, "UTF-8");
+			if(msgS.contains("Ola")) {
+				resp = "Ola, o que desejas?\n1. Pesquisas\n2.Armas\n3.Lula";
+			}if(msgS.contains("1")) {
+				resp = "Ninguem vai ganhar nem vai perder.\nnem quem ganhar nem quem perder vai ganhar ou perder"
+						+ "\nvai todo mundo perder.";
+			}if(msgS.contains("2")) {
+				resp = "Pistola Tauros 12x de 39,90 na Casa Bahia \nmas so ate amanhã";
+			}if(msgS.contains("3")) {
+				resp = "Lula ladrão, roubou meu coração";
+			}
+			sendData = (resp).getBytes();
 			System.out.println(msgS);
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIP, port);
 			serverSocket.send(sendPacket);
